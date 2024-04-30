a3031370a52b84e6e6c8a27f20f6c1b7aaa7762e
Igor Zalutski <izalutski@gmail.com>
2024-04-30 21:35:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-variable "input_var" {
+variable "INPUT_VAR" {
   description = "An input variable"
   type        = string
   default     = "Hello, Terraform!"
 }
 
 output "variable_output" {
-  value       = var.input_var
+  value       = var.INPUT_VAR
   description = "This output shows the value of the input variable."
 }
-
